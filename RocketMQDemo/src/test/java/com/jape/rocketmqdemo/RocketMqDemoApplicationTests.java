1072b4ad6cfdcacf41b3a70d6fabaff5f239038e
WangJape <1143828481@qq.com>
2021-03-18 22:43:11 +0800

    增加RocketMqDemo

---
+++ RocketMQDemo/src/test/java/com/jape/rocketmqdemo/RocketMqDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jape.rocketmqdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RocketMqDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
